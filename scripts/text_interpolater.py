c7c671cb30e338aa3f36ba7deda0d30d92586f91
michael menietti <mmenietti@hbs.edu>
2020-01-21 11:24:36 -0500

    Add directory support to text interpolator

--- scripts/text_interpolater.py
+++ scripts/text_interpolater.py
@@ -6,6 +6,22 @@ import os
 import argparse
 
 #------------------------------------------------------------
+# File Contents Reader
+#------------------------------------------------------------
+def collect_insertion_data(insert_root):
+    inserts_dict = {}
+    for x in insert_root.iterdir():
+        if x.is_file():
+            inserts_dict[x.stem] = x.read_text(encoding='utf-8')
+        elif x.is_dir():
+            sub_inserts = collect_insertion_data(x)
+            for (path_stem, file_contents) in sub_inserts.items():
+                insert_key = x.stem + '/' + path_stem
+                inserts_dict[insert_key] = file_contents
+
+    return inserts_dict
+
+#------------------------------------------------------------
 # Entry Point
 #------------------------------------------------------------
 if __name__ == "__main__":
@@ -23,10 +39,13 @@ if __name__ == "__main__":
     output_file_path       = pathlib.Path(args.output_file)  
 
     # Collect insertion data
-    inserts_dict = {}
-    for x in inserts_directory_path.iterdir():
-        if x.is_file():
-            inserts_dict[x.stem] = x.read_text(encoding='utf-8')
+    # inserts_dict = {}
+    # for x in inserts_directory_path.iterdir():
+    #     if x.is_file():
+    #         inserts_dict[x.stem] = x.read_text(encoding='utf-8')
+    inserts_dict = collect_insertion_data(inserts_directory_path)
+
+    # print(inserts_dict)
 
     # Import the input file text and interpolate the insertion text
     input_file_string = input_file_path.read_text(encoding='utf-8')
